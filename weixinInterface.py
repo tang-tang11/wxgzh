92b9fe26b1422f130ffe083263c08bd16ba12e75
tang-tang11 <100356360+tang-tang11@users.noreply.github.com>
2022-09-08 19:32:57 +0800

    Update weixinInterface.py

--- weixinInterface.py
+++ weixinInterface.py
@@ -21,7 +21,7 @@ class WeixinInterface:
         nonce=data.nonce
         echostr = data.echostr
         #自己的token
-        token="token" #这里改写你在微信公众平台里输入的token
+        token="091114" #这里改写你在微信公众平台里输入的token
         #字典序排序
         list=[token,timestamp,nonce]
         list.sort()
